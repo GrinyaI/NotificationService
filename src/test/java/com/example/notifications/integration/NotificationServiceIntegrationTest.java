78add3555b65bf6b0c315be85fc51bf662f27cc0
Ilya <grinevich2018@mail.ru>
2025-07-12 01:13:30 +0600

    Add test

--- src/test/java/com/example/notifications/integration/NotificationServiceIntegrationTest.java
+++ src/test/java/com/example/notifications/integration/NotificationServiceIntegrationTest.java
@@ -18,10 +18,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
